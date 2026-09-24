073ad357cec15d23df40bf9bd609f4406f448033
Oscar Noé Fuentes Rabago 331-11 <oscarnoefuentesrabago@gmail.com>
2026-09-24 15:51:06 -0700

    Add files via upload

---
+++ CS2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+// Espacio de nombres
+namespace CS2
+{
+    // Clase principal
+    class Program
+    {
+        // Función principal
+        static void Main(string[] args)
+        {
+            // Sesión 5: Tipos de datos
+            // Sintaxis para declarar variables
+            // Tipo_de_dato identificador_variable;
+            // 1. Entero
+            int a;
+            // 2. Cadena de texto
+            string s;
+            // 3.1 Flotantes (precisión sencilla)
+            float f;
+            // 3.2 Flotantes (precisión doble)
+            double d;
+            // 4. Booleano (lógico)
+            bool b;
+            // Inicializaciones
+            a = 5;
+            s = "Exactas";
+            f = 8.5F;
+            d = 9.5D;
+            b = true;
+            // Palabra reservada: Identificaddor especial predefinido para el compilador.
+            // Interpolación: Combinacion de datos dentro de una cadena.
+            // Impresiones
+            Console.WriteLine($"Entero: {a}");
+            Console.WriteLine($"Flotante (precisión sencilla): {f}");
+            Console.WriteLine($"Flotante (precisión doble): {d}");
+            Console.WriteLine($"Cadena de texto: {s}");
+            Console.WriteLine($"Booleano: {b}");
+        } // Término de la función principal
+    } // Término de la clase pricipal
+} // Término del espacio de nombres

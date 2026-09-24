19cb928091ec817dc0602c536b5e71bcdbeff40a
Oscar Noé Fuentes Rabago 331-11 <oscarnoefuentesrabago@gmail.com>
2026-09-24 16:24:21 -0700

    Add files via upload

---
+++ SE1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace SE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // 1. Proyecto en C#
+           // 2. Sintaxis
+           // Tipo_de_dato indetificador_variable = valor_inicial;
+           bool a;
+           int numero;
+           // 3. Interpolacion
+           // Combinacion de datos dentro de una cadena.
+           a = true;
+           numero = 10;
+           Console.WriteLine($"Booleano: {a}");
+           Console.WriteLine($"Número: {numero}");
+           // 4. Incrementos y decrementos
+           int m = 0;
+           int n = -1;
+           m += 1;
+           n -= 3;
+           m -= 5;
+           n += 9;
+           // 5. Operador resto (módulo)
+           int residuo = 40 % 16;
+           Console.WriteLine($"Residuo: {residuo}");
+           // 6. Operadores Aritmeticos
+           double operación = 0;
+           operación =((30 + 8 -2) / 2)* -1;
+           Console.WriteLine($"Operación: {operación}");
+           // Sesión 11: Solucion de prolemas (continuación)
+           // 7. Interrumptores
+           bool interuptor_1 = false;
+           bool interuptor_2 = false;
+           bool bombilla = false;
+           if (interuptor_1 && interuptor_2)
+            {
+                bombilla = true;
+            }
+            else
+            {
+                bombilla = false;
+            }
+            Console.WriteLine($"Bombilla: {bombilla}");
+            // 8. Asueto
+               int dia = 16;
+               string mes = "Septiembre";
+               if(dia == 16 && mes == "Septiembre")
+               {
+                Console.WriteLine("Hay asueto.");
+               }
+               else
+               {
+                Console.WriteLine("Sin definir");
+               }
+
+               bool resultado = (7 > 11) && 9 != 0;
+               Console.WriteLine($"Resultado: {resultado}");
+        }       
+    }
+}
